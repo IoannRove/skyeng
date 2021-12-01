09b596f1c868d7ebb2294c13754e6016dc179cb4
ivan <ivan.shaibakov@gmail.com>
2021-12-01 19:19:38 +0300

    Type hint

--- backend/model/utils.py
+++ backend/model/utils.py
@@ -1,7 +1,8 @@
 import json
 import pathlib
+from typing import Union, List
 
 
-def get_json_from_file(file_name: str):
+def get_json_from_file(file_name: str) -> Union[List[dict], dict]:
     with open(pathlib.Path(__file__).parent / 'data' / file_name, 'rb') as f:
         return json.load(f)
